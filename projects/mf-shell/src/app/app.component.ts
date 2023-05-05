b8679a2e20b327ea33aa047e185d8e879f2e0c4f
Diego Pagini <diego.pagini@neoris.com>
2023-05-05 13:58:17 +0200

    update

--- projects/mf-shell/src/app/app.component.ts
+++ projects/mf-shell/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonLibService } from '@common-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mf-shell';
+
+  constructor(private readonly _lib: CommonLibService) {}
 }
--- projects/mf-shell/webpack.config.js
+++ projects/mf-shell/webpack.config.js
@@ -1,14 +1,18 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-
   remotes: {
-    "mfShopping": "http://localhost:4200/remoteEntry.js",
-    "mfPayment": "http://localhost:4200/remoteEntry.js",    
+    mfShopping: "http://localhost:4201/remoteEntry.js",
+    mfPayment: "http://localhost:4202/remoteEntry.js",
   },
-
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
